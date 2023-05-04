25cdb18e1124affcc1eb1db4ddeda7a4d0848dc0
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-04 13:21:12 +0530

    Add user to firestore

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -6,7 +6,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
@@ -43,6 +43,12 @@ function SignUp() {
     updateProfile(auth.currentUser, {
       displayName: name,
     });
+
+    const formDataCopy = { ...formData };
+    delete formDataCopy.password;
+
+    formDataCopy.timestamp = serverTimestamp();
+    await setDoc(doc(db, "users", user.uid), formDataCopy);
     navigate("/");
   };
 
