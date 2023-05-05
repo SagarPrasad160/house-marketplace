b55dfd00a9a7a2c40a54477658f306074c6972b2
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-05 10:01:41 +0530

    Add PrivateRoute Component and useAuthStatus

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Offer from "./pages/Offer";
 import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Explore from "./pages/Explore";
@@ -17,7 +18,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offer />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
