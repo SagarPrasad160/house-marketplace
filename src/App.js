d56e5f016db08445cf3e8849d5d6b3249d76d749
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-06 13:14:29 +0530

    Add Category Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Offer from "./pages/Offer";
 import Profile from "./pages/Profile";
+import Category from "./pages/Category";
 import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offer />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
